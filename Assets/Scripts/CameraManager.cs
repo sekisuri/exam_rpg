862590c0bfb1875d90b505fb3abc409e0061a432
sekisuri <sekisuri@gmail.com>
2019-09-01 01:08:34 +0900

    Part 4 : 카메라 움직이기

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject target;
+    public float moveSpeed;
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
