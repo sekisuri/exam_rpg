3c50934284a1a55de2e618fdbb436aac9cc7abdb
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-24 23:56:35 +0900

    Part 13 : 이벤트 (2) - 대화 시스템

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+
+    #region Singleton
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+
+    public Text text;
+    public SpriteRenderer rendererSprite; // sprite - audioclip
+    public SpriteRenderer rendererDialogueWindow;
+
+    private List<string> listSentences;
+    private List<Sprite> listSprites;
+    private List<Sprite> listDialogueWindows;
+
+    private int count; // 대화 진행 상황 카운트
+
+    public Animator animSprite;
+    public Animator animDialogueWindow;
+
+    public string typeSound;
+    public string enterSound;
+
+    private AudioManager theAudio;
+
+    public bool talking = false;
+    private bool keyActivated = false;
+
+    private void Start()
+    {
+        count = 0;
+        text.text = "";
+        listSentences = new List<string>();
+        listSprites = new List<Sprite>();
+        listDialogueWindows = new List<Sprite>();
+
+        theAudio = FindObjectOfType<AudioManager>();
+
+    }
+
+    public void ShowDialogue(Dialogue dialogue)
+    {
+        talking = true;
+        for(int i = 0; i < dialogue.sentences.Length; i++)
+        {
+            listSentences.Add(dialogue.sentences[i]);
+            listSprites.Add(dialogue.sprites[i]);
+            listDialogueWindows.Add(dialogue.dialogueWindows[i]);
+
+        }
+
+        animSprite.SetBool("Appear", true);
+        animDialogueWindow.SetBool("Appear", true);
+        StartCoroutine(StartDialogueCoroutine());
+#if true
+        Debug.Log("쇼우 다이얼로그");
+#endif
+    }
+
+    public void ExitDialogue()
+    {
+        text.text = "";
+        count = 0;
+        listSentences.Clear();
+        listSprites.Clear();
+        listDialogueWindows.Clear();
+        animSprite.SetBool("Appear", false);
+        animDialogueWindow.SetBool("Appear", false);
+        talking = false;
+#if true
+        Debug.Log("엑스트 다이얼로그");
+#endif
+    }
+    IEnumerator StartDialogueCoroutine()
+    {
+
+        if(count > 0)
+        {
+            if (listDialogueWindows[count] != listSprites[count - 1])
+            {
+                animSprite.SetBool("Change", true);
+                animDialogueWindow.SetBool("Appear", false);
+                yield return new WaitForSeconds(0.2f);
+                rendererDialogueWindow.GetComponent<SpriteRenderer>().sprite = listDialogueWindows[count];
+                rendererSprite.GetComponent<SpriteRenderer>().sprite = listSprites[count];
+                animDialogueWindow.SetBool("Appear", true);
+                animSprite.SetBool("Change", false);
+
+#if true
+                Debug.Log("첫번째 이프문");
+#endif
+
+            }
+            else
+            {
+                if (listSprites[count] != listSprites[count - 1])
+                {
+                    animSprite.SetBool("Change", true);
+                    yield return new WaitForSeconds(0.1f);
+                    rendererSprite.GetComponent<SpriteRenderer>().sprite = listSprites[count];
+                    animSprite.SetBool("Change", false);
+#if true
+                    Debug.Log("엘스문 안에 이프문");
+#endif
+                }
+                else
+                {
+#if true
+                    Debug.Log("엘스문 안에 엘스문");
+#endif
+                    yield return new WaitForSeconds(0.05f);
+                }
+            }
+        }
+        else
+        {
+#if true
+            Debug.Log("첫번째 엘스문");
+#endif
+            rendererDialogueWindow.GetComponent<SpriteRenderer>().sprite = listDialogueWindows[count];
+            rendererSprite.GetComponent<SpriteRenderer>().sprite = listSprites[count];
+        }
+        keyActivated = true;
+       for(int i = 0; i< listSentences[count].Length; i++)
+        {
+            text.text += listSentences[count][i]; // 1글자씩 출력
+
+            if( i % 7 == i)
+            {
+                theAudio.Play(typeSound);
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (talking && keyActivated)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                count++;
+                text.text = "";
+                keyActivated = false;
+
+                theAudio.Play(enterSound);
+
+                if (count == listSentences.Count)
+                {
+                    StopAllCoroutines();
+                    ExitDialogue();
+#if true
+                    Debug.Log("업데이트문 안에 이프문  : " + count);
+#endif
+                }
+                else
+                {
+#if true
+                    Debug.Log("업데이트문 안에 엘스문 : " + count);
+#endif
+                    StopAllCoroutines();
+                    StartCoroutine(StartDialogueCoroutine());
+                }
+            }
+        }
+       
+    }
+}
