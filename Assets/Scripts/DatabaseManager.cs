de22b7e209344862837198a3afaaff904f3c8bf3
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-26 16:29:51 +0900

    Part 17 : DB 매니저

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+
+    static public DatabaseManager instance;
+    public string[] var_name;
+    public float[] var_f;
+
+    public string[] switch_name;
+    public bool[] switches;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+
+}
