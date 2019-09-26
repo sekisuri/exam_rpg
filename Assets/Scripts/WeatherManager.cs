227d65025eaeef8798be7976cec14cddde1af331
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-26 16:02:29 +0900

    Part 16 : 날씨 효과 (비)

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    static public WeatherManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private AudioManager theAudio;
+    public ParticleSystem rain;
+
+    private void Start()
+    {
+        theAudio = FindObjectOfType<AudioManager>();
+    }
+
+    public void Rain()
+    {
+        rain.Play();
+    }
+
+    public void RainStop()
+    {
+        rain.Stop();
+    }
+}
