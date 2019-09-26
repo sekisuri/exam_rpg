734ed7ccf0d8fabb58f2d6494abb7ea57ba95ddf
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-26 16:44:10 +0900

    Part 18 : 라이트 시스템

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    private PlayerManager thePlayer;
+    private Vector2 vector;
+
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerManager>();
+    }
+
+    private void Update()
+    {
+        this.transform.position = thePlayer.transform.position;
+        vector.Set(thePlayer.animator.GetFloat("DirX"), thePlayer.animator.GetFloat("DirY"));
+
+        if(vector.x == 1f)
+        {
+            rotation = Quaternion.Euler(0, 0, 90);
+            this.transform.rotation = rotation;
+        }
+        else if(vector.x == -1f)
+        {
+            rotation = Quaternion.Euler(0, 0, -90);
+            this.transform.rotation = rotation;
+        }
+        else if(vector.y == 1f)
+        {
+            rotation = Quaternion.Euler(0, 0, 180);
+            this.transform.rotation = rotation;
+        }
+        else if(vector.y == -1f)
+        {
+            rotation = Quaternion.Euler(0, 0, 0);
+            this.transform.rotation = rotation;
+        }
+
+    }
+}
