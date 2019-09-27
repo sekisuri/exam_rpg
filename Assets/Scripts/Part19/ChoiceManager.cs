932ca7f02a113c44c7814a1740054524bee7faf5
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-27 15:11:06 +0900

    Part 19 : 선택지 시스템 (분기), 폴더 변경

---
+++ Assets/Scripts/Part19/ChoiceManager.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceManager : MonoBehaviour
+{
+    public static ChoiceManager instance;
+
+    private AudioManager theAudio;
+    private string question;
+    private List<string> answerList;
+
+    public GameObject go;
+
+    public Text question_Text;
+    public Text[] answer_Text;
+
+    public GameObject[] answer_Panel;
+    public string keySound;
+    public string enterSound;
+
+    public bool choiceIng;
+    private bool keyInput;
+
+
+    private int count;
+
+    private int result;
+    public Animator anim;
+
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+
+    private void Start()
+    {
+        theAudio = FindObjectOfType<AudioManager>();
+        answerList = new List<string>();
+        for(int i = 0; i <= answer_Text.Length; i++)
+        {
+            answer_Text[i].text = "";
+            answer_Panel[i].SetActive(false);
+        }
+
+        question_Text.text = "";
+    }
+
+    public void ShowChoice(choice _choice)
+    {
+        go.SetActive(true);
+        result = 0;
+        question = _choice.question;
+        for(int i = 0; i< _choice.answers.Length; i++)
+        {
+            answerList.Add(_choice.answers[i]);
+            answer_Panel[i].SetActive(true);
+            count = i;
+        }
+        anim.SetBool("Appear", true);
+        Selection();
+        StartCoroutine(ChoiceCoroutine());
+    }
+
+    IEnumerator ChoiceCoroutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(TypingQuestion());
+        StartCoroutine(TypingAnswer_0());
+
+        if(count >= 1)
+        {
+            StartCoroutine(TypingAnswer_1());
+        }
+        if(count >= 2)
+        {
+            StartCoroutine(TypingAnswer_2());
+        }
+        if (count >= 3)
+        {
+            StartCoroutine(TypingAnswer_3());
+        }
+        yield return new WaitForSeconds(0.5f);
+        keyInput = true;
+    }
+
+    IEnumerator TypingQuestion()
+    {
+        for(int i = 0; i < question.Length; i++)
+        {
+            question_Text.text += question[i];
+            yield return waitTime;
+        }
+    }
+
+    IEnumerator TypingAnswer_0()
+    {
+        yield return new WaitForSeconds(0.4f);
+        for(int i = 0; i < answerList[0].Length; i++)
+        {
+            answer_Text[0].text += answerList[0][i];
+            yield return waitTime;
+        }
+    }
+
+    IEnumerator TypingAnswer_1()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < answerList[1].Length; i++)
+        {
+            answer_Text[1].text += answerList[1][i];
+            yield return waitTime;
+        }
+    }
+    IEnumerator TypingAnswer_2()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < answerList[2].Length; i++)
+        {
+            answer_Text[2].text += answerList[2][i];
+            yield return waitTime;
+        }
+    }
+
+    IEnumerator TypingAnswer_3()
+    {
+        yield return new WaitForSeconds(0.6f);
+        for (int i = 0; i < answerList[3].Length; i++)
+        {
+            answer_Text[3].text += answerList[3][i];
+            yield return waitTime;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (keyInput)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                theAudio.Play(keySound);
+                if(result > 0)
+                {
+                    result--;
+                }
+                else
+                {
+                    result = count;
+                }
+                Selection();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                theAudio.Play(keySound);
+                if(result < count)
+                {
+                    result++;
+                }
+                else
+                {
+                    result = 0;
+                }
+
+                Selection();
+            }
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                theAudio.Play(enterSound);
+                keyInput = false;
+                ExitChoice();
+            }
+
+        }
+    }
+
+    public void ExitChoice()
+    {
+        for(int i = 0; i <= count; i++)
+        {
+            answer_Text[i].text = "";
+            answer_Panel[i].SetActive(false);
+        }
+
+        
+        question_Text.text = "";
+        anim.SetBool("Appear", false);
+        choiceIng = false;
+    }
+    public void Selection()
+    {
+        Color color = answer_Panel[0].GetComponent<Image>().color;
+        color.a = 0.75f;
+        for(int i = 0; i <= count; i++)
+        {
+            answer_Panel[i].GetComponent<Image>().color = color;
+        }
+        color.a = 1f;
+    }
+
+    public int GetResult()
+    {
+        return result;
+    }
+}
