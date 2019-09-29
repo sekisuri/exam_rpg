5de029a72c3c48a9fd29f3d4a6aa891e8019cf7a
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-29 13:17:44 +0900

    part 23 아이템 구현

--- Assets/Scripts/Manager/DatabaseManager.cs
+++ Assets/Scripts/Manager/DatabaseManager.cs
@@ -12,6 +12,7 @@ public class DatabaseManager : MonoBehaviour
     public string[] switch_name;
     public bool[] switches;
 
+    public List<Item> itemList = new List<Item>();
     private void Awake()
     {
         if(instance != null)
@@ -23,6 +24,11 @@ public class DatabaseManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
             instance = this;
         }
+    }
+
+    private void Start()
+    {
+        itemList.Add(new Item(10001, "빡간 포션", "체력을 50 채워주는 마법의 물약", Item.ItemType.Use));
     }
 
 }
