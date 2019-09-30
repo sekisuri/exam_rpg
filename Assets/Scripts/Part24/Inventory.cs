fe683b14bf85c6dec3b5bb903eb5e3dfa48ace0a
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-30 18:14:21 +0900

    part 24 인벤토리 시스텝

--- Assets/Scripts/Part24/Inventory.cs
+++ Assets/Scripts/Part24/Inventory.cs
@@ -154,9 +154,93 @@ public class Inventory : MonoBehaviour
                 else if (Input.GetKeyDown(KeyCode.Z))
                 {
                     theAudio.Play(enter_sound);
+                    Color color = selectedTabImages[selectedTab].GetComponent<Image>().color;
+                    color.a = 0.25f;
+                    selectedTabImages[selectedTab].GetComponent<Image>().color = color;
+                    itemActivated = false;
+                    tabActivated = false;
+                    preventExec = true;
+                    ShowItem();
                 }
 
             }
+        }
+    }
+
+    public void ShowItem()
+    {
+        inventoryTabList.Clear();
+        RemoveSlot();
+        selectedItem = 0;
+
+        switch (selectedTab)
+        {
+            case 0:
+                for(int i = 0;i < inventoryTabList.Count; i++)
+                {
+                    if(Item.ItemType.Use == inventoryTabList[i].itemType)
+                    {
+                        inventoryTabList.Add(inventoryTabList[i]);
+                    }
+                    
+                }
+                break;
+            case 1:
+                for(int i = 0; i < inventoryTabList.Count; i++)
+                {
+                    if(Item.ItemType.Equip == inventoryTabList[i].itemType)
+                    {
+                        inventoryTabList.Add(inventoryTabList[i]);
+                    }
+                }
+                break;
+            case 2:
+                for(int i = 0;i < inventoryTabList.Count; i++)
+                {
+                    if(Item.ItemType.Quest == inventoryTabList[i].itemType)
+                    {
+                        inventoryTabList.Add(inventoryTabList[i]);
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < inventoryTabList.Count; i++)
+                {
+                    if (Item.ItemType.Quest == inventoryTabList[i].itemType)
+                    {
+                        inventoryTabList.Add(inventoryTabList[i]);
+                    }
+                }
+                break;
+        } // 탭에 따른 아이템 분류. 그것을 인벤토리 탭 리스트에 추가
+
+        for(int i = 0; i < inventoryTabList.Count; i++)
+        {
+            slots[i].gameObject.SetActive(true);
+            slots[i].Additem(inventoryTabList[i]);
+        } //인벤토리 탭 리스트의 내용을 인벤토리에 추가
+
+        SelectedItem();
+    }
+
+    public void SelectedItem()
+    {
+        StopAllCoroutines();
+        if(inventoryTabList.Count > 0)
+        {
+            Color color = slots[0].selected_Item.GetComponent<Image>().color;
+            Description_Text.text = inventoryTabList[selectedItem].itemDescription;
+            color.a = 0f;
+            for(int i = 0; i < inventoryItemList.Count; i++)
+            {
+                slots[i].selected_Item.GetComponent<Image>().color = color;
+            }
+            Description_Text.text = inventoryItemList[selectedItem].itemDescription;
+            StartCoroutine(SelectedTabEffectCoroutine());
+        }
+        else
+        {
+            Description_Text.text = "해당 타입의 아이템을 소유하고 있지 않습니다.";
         }
     }
 
